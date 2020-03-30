845b06f2187f8601ca84dbaec3cdadc8a27dedb2
Harish Tg <harishtgtg@gmail.com>
2020-03-30 22:15:19 +0530

    simple wether api call

---
+++ app.js
@@ -0,0 +1,7 @@
+const request = require('request'); // npm module 
+const url = 'https://api.darksky.net/forecast/abd629a2a92d47e22e5f79dd28936f76/37.8267,-122.4233' // url or the weather
+
+request({url:url /* providing url */, json: true /* percing the respond data from json to object is set to true */}, (error,respond) => {
+console.log(`${respond.body.daily.summary} It is currently ${respond.body.currently.temperature} degree out`);
+
+}) // the npm module request takes two arguments the first arguments is for controle which is in a object formet and second is the callback funcion which responds error and the data
