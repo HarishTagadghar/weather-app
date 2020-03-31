b7966dbf51663eac7d6ec4459c6ddeab855a0a10
Harish Tg <harishtgtg@gmail.com>
2020-03-31 16:25:20 +0530

    updated forecast module and made the program terminal freandly

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const request = require('request'); // npm module 
 const chalk = require('chalk');
 const geocode = require('./utils/geocode')
-const url = 'https://api.darksky.net/forecast/abd629a2a92d47e22e5f79dd28936f76/37.8267,-122.4233' // url or the weather
-
-// request to get the weather data
-request({url:url /* providing url */, json: true /* percing the respond data from json to object is set to true */}, (error,respond) => {
-    if(error){
-        console.log(chalk.red.inverse('server error to call weather api'));
-    }else if (respond.body.error){
-        console.log(chalk.red.inverse('incorrect location'));
-    }else{
-        console.log(chalk.green(`${respond.body.daily.summary} It is currently ${respond.body.currently.temperature} degree out`));
-    }
-}) // the npm module request takes two arguments the first arguments is for controle which is in a object formet and second is the callback funcion which responds error and the data
+const forecast = require('./utils/forecast')
 
 // request to get the location latitude longitude
 
-geocode('homnabad',(error,data) => {
-    console.log('error:' + error);
-    console.log('data:' + data.location);
-    
-})
+let address = process.argv[2] 
+if(!address){
+    console.log('provide a address');
+}else{
+    geocode(address  ,(error,data) => {
+        if(error){
+           return console.log(error);
+        }
+            forecast(data.longitude,data.latitude,(error,forecastdata) => {
+                if (error){
+                    return console.log(error);    
+                }
+                console.log(data.location);
+                console.log(forecastdata);      
+            })
+        })
+        
+        
+}
